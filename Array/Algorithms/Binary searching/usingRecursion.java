e7d17dc8ffe30a057f19927be079b9c63e25165a
sattupawar <satyampawar0070@gmail.com>
2024-01-05 19:21:08 +0530

    learn binary searching intro by striver

---
+++ Array/Algorithms/Binary searching/usingRecursion.java
@@ -0,0 +1,25 @@
+public class usingRecursion {
+
+  public static void main(String[] args) {
+    
+  }
+
+  static int findElement(int[] a, int element) {
+    return binary(a, 0, a.length - 1, element);
+  }
+
+  static int binary(int[] a, int start, int end, int element) {
+    int mid = start + (end - start) / 2;
+
+    if (start > end) {
+      return -1;
+    }
+    if (a[mid] == element) {
+      return mid;
+    } else if (element < a[mid]) {
+      return binary(a, start, mid - 1, element);
+    } else {
+      return binary(a, start + 1, end, element);
+    }
+  }
+}
